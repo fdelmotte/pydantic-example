063680710b3ba50348eaa190220b93c3d579da33
dwarf <fd@arista.com>
2022-07-28 08:36:46 +0200

    add load_json_version to the class DeviceModel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 from pydantic import BaseModel
 from typing import Optional, List, Dict
+from rich.pretty import pprint
 
 
 class TempSensor(BaseModel):
@@ -31,9 +32,19 @@ class PowerSupply(BaseModel):
     managed: Optional[bool]
 
 
+class Version(BaseModel):
+    hostName: str
+    name: Optional[str]
+    version: Optional[str]
+    terminattr: Optional[str]
+    model: Optional[str]
+    serial_number: Optional[str]
+
+
 class DeviceModel():
     def __init__(self) -> None:
         self.psus = []
+        self.versions = []
 
 
     def _load_json_file(self, file_path: str):
@@ -52,9 +63,9 @@ class DeviceModel():
 
 
     def load_json_psus(self, json_file: str):
-        print(type(self._load_json_file(json_file)))
         for device_hostname, value in self._load_json_file(json_file):
             for k, entry in value[0]['powerSupplies'].items():
+                print(type(entry))
                 entry['hostName'] = device_hostname
                 entry['id'] = int(k)
                 entry = self._dict_to_list(entry=entry, key='tempSensors')
@@ -62,9 +73,18 @@ class DeviceModel():
                 self.psus.append(PowerSupply(**entry))
 
 
+    def load_json_version(self, json_file: str):
+        for device_hostname, value in self._load_json_file(json_file):
+            for k, entry in value[0].items():
+                print("line : {}".format(type(entry)))
+                # entry['hostName'] = device_hostname
+            #     self.versions.append(Version(**entry))
+
 if __name__ == '__main__':
     eos = DeviceModel()
     eos.load_json_psus(json_file='files/show_env_power.json')
-    for psu in eos.psus:
-        print(psu.dict(), expand_all=False)
+    eos.load_json_version(json_file='files/devices_global.json')
+
+    # for psu in eos.psus:
+    #     pprint(psu.dict(), expand_all=False)
 
