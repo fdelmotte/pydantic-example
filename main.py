4f1fbd55445fd1d149c61f0eb6bf2f8722152b93
dwarf <fd@arista.com>
2022-07-28 12:35:50 +0200

    load_json_version module OK

--- main.py
+++ main.py
@@ -34,11 +34,12 @@ class PowerSupply(BaseModel):
 
 class Version(BaseModel):
     hostName: str
-    name: Optional[str]
     version: Optional[str]
     terminattr: Optional[str]
-    model: Optional[str]
-    serial_number: Optional[str]
+    modelName: Optional[str]
+    serialNumber: Optional[str]
+    isIntlVersion: Optional[str]
+    switchType: Optional[str]
 
 
 class DeviceModel():
@@ -56,7 +57,10 @@ class DeviceModel():
         if key in entry:
             temp_list = []
             for ct, sensor in entry[key].items():
-                sensor['name'] = ct
+                try:
+                    sensor['name'] = ct
+                except TypeError:
+                    pass
                 temp_list.append(sensor)
             entry[key] = temp_list
         return entry
@@ -65,7 +69,6 @@ class DeviceModel():
     def load_json_psus(self, json_file: str):
         for device_hostname, value in self._load_json_file(json_file):
             for k, entry in value[0]['powerSupplies'].items():
-                print(type(entry))
                 entry['hostName'] = device_hostname
                 entry['id'] = int(k)
                 entry = self._dict_to_list(entry=entry, key='tempSensors')
@@ -75,16 +78,20 @@ class DeviceModel():
 
     def load_json_version(self, json_file: str):
         for device_hostname, value in self._load_json_file(json_file):
-            for k, entry in value[0].items():
-                print("line : {}".format(type(entry)))
-                # entry['hostName'] = device_hostname
-            #     self.versions.append(Version(**entry))
+            entry = value[0]
+            entry['hostName'] = device_hostname
+            entry['switchType'] = entry.get('details').get('switchType')
+            entry['terminattr']= entry.get('details').get('packages').get('TerminAttr-core').get('version')
+            self.versions.append(Version(**entry))
 
 if __name__ == '__main__':
     eos = DeviceModel()
     eos.load_json_psus(json_file='files/show_env_power.json')
     eos.load_json_version(json_file='files/devices_global.json')
 
-    # for psu in eos.psus:
-    #     pprint(psu.dict(), expand_all=False)
+    for psu in eos.psus:
+        pprint(psu.dict(), expand_all=False)
+
+    for version in eos.versions:
+        pprint(version.dict(), expand_all=False)
 
